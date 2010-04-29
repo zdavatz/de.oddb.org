b7ac0c5ffd7100aa98c99c0fe5c204c60b3cc498
Hannes Wyss <hwyss@ywesee.com>
2010-04-29 15:52:11 +0200

    Ensure Date and Time information gets logged
    The PayPal Gem depends on ActiveSupport, which foolishly redefines the
    Logger.default_formatter. That's why we need to explicitly set
    logger.formatter to the standard Formatter.

--- lib/oddb/util/logger.rb
+++ lib/oddb/util/logger.rb
@@ -10,5 +10,10 @@ module ODDB
   end
   logger = Logger.new(log_file)
   logger.level = Logger.const_get(@config.log_level)
+  ## The PayPal Gem depends on ActiveSupport, which foolishly redefines the
+  #  Logger.default_formatter. That's why we need to explicitly set
+  #  logger.formatter to the standard Formatter here:
+  logger.formatter = Logger::Formatter.new
   @logger = logger
+  require 'pp'
 end
