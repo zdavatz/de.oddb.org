ad985bd59a59795fa7311352dcdef731007cac2d
Zeno R.R. Davatz <zdavatz@ywesee.com>
2010-12-21 11:12:10 +0100

    Added Version to lib/oddb.rb

--- lib/oddb.rb
+++ lib/oddb.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # ODDB -- de.oddb.org -- 27.10.2006 -- hwyss@ywesee.com
 
+VERSION = '2.0.0'
+
 module ODDB
   ODDB_VERSION =
     File.read(File.expand_path('../.git/refs/heads/master',
